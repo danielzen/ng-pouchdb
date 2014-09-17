6bc5041560b5a2586f640c6e4ad236febf7eb43a
Daniel Zen <daniel@zendigital.com>
2014-09-17 12:57:36 -0400

    Removed need for CouchDb

--- demo/app.js
+++ demo/app.js
@@ -1,8 +1,8 @@
 angular.module('app', ['pouchdb'])
 .controller('App', function($scope, pouchCollection, pouchBindingSimple) {
-    $scope.books = pouchCollection('http://localhost:5984/books');
+    $scope.books = pouchCollection('books');
     $scope.userInfo = {
     firstName: 'Jo', lastName: 'Bloggs'
   };
-  pouchBindingSimple('http://localhost:5984/user-info', $scope, 'userInfo');
+  pouchBindingSimple('user-info', $scope, 'userInfo');
 });
